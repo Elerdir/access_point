2e72e77831cd0bd3599acbf25541f5f14a93770f
Ladislav Niderle <niderle.l@seznam.cz>
2023-10-27 05:08:17 +0200

    doplnění crossorigin

--- server/src/main/java/cz/ess/server/user/registration/RegistrationController.java
+++ server/src/main/java/cz/ess/server/user/registration/RegistrationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/registration")
+@CrossOrigin("*")
 public class RegistrationController {
 
     private final RegistrationService registrationService;
