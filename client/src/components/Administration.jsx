3c054d55eb4f277f32a8736017b35b872c8c010a
Ladislav Niderle <niderle.l@seznam.cz>
2023-11-22 11:10:02 +0100

    oprava volání seznamu všech uživatelů jak admin a načtení všech informací o uživatelých

--- client/src/components/Administration.jsx
+++ client/src/components/Administration.jsx
@@ -7,25 +7,31 @@ import AuthContext from "../context/AuthProvider";
 export const Administration = () => {
 	const {auth} = useContext(AuthContext);
 	const [allUsers, setAllUsers] = useState([]);
+	const [allApps, setAllApps] = useState([]);
 
 	const fetchAllUsers = async () => {
 		try {
+			const headers = {
+				"Content-Type": "application/json",
+				'Authorization': 'SJKNFM?NK-DFGVV-FDFVD5-vdF52-dFDF'
+			};
+
 			const response = await axios.post(
 				"/user/all",
+				{},
 				{
-					// headers: {'Accept': 'application/json', 'Authorization': `Basic ${auth.token}`},
-					header: {'Accept': 'application/json', 'Authorization': 'SJKNFM?NK-DFGVV-FDFVD5-vdF52-dFDF'},
+					headers: headers,
 					// withCredentials: false,
 					mode: 'no-cors',
-					// Accept: 'application/json',
-					// Authorization: 'SJKNFM?NK-DFGVV-FDFVD5-vdF52-dFDF'
 
 				}
 			);
 
-			console.log(response?.data)
-			setAllUsers(response?.data);
+			console.log('data ok: ', response?.data.users)
+			setAllUsers(response?.data.users);
+			setAllApps(response?.data.apps);
 		} catch (err) {
+			console.log('data err: ',err);
 			console.log(err?.header)
 			console.log(err?.response?.message)
 		}
