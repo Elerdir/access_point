7bf50a57e0ed1fcaa0b57b2ea24784c9c4bf3719
Matěj <mckillem@tuta.io>
2023-11-24 17:05:43 +0100

    oprava

--- client/src/components/Signpost.tsx
+++ client/src/components/Signpost.tsx
@@ -13,7 +13,6 @@ export const Signpost = () => {
 				<h2>Vyber si službu</h2>
 				{userObject?.userO?.user?.userToApps ?
 					userObject?.userO?.apps.filter(({ id }: {id: number}) => {
-						console.log(userObject?.userO?.user?.userToApps.map(({appId}: {appId: number}) => appId))
 						return id === userObject?.userO?.user?.userToApps.map(({appId}: {appId: number}) => appId)[0]
 					}).map(({ appName, url }: {appName: number, url: number}) => {
 						return <p><a href={`${url}`}>{appName}</a></p>
